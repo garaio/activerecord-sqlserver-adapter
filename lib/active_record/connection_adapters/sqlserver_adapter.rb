5e07767c0f0dada18a6f88f95f142683a2e659dd
Michael Stämpfli <michael.staempfli@garaio.com>
2011-09-29 08:49:05 +0200

    SQL Server 2000 is partially supported. Don't use find_in_batches since the TOP operator doesn't exist yet.

--- lib/active_record/connection_adapters/sqlserver_adapter.rb
+++ lib/active_record/connection_adapters/sqlserver_adapter.rb
@@ -13,9 +13,9 @@ require 'active_support/core_ext/string'
 require 'base64'
 
 module ActiveRecord
-  
+
   class Base
-    
+
     def self.sqlserver_connection(config) #:nodoc:
       config = config.symbolize_keys
       config.reverse_merge! :mode => :dblib, :host => 'localhost', :username => 'sa', :password => ''
@@ -33,21 +33,21 @@ module ActiveRecord
       end
       ConnectionAdapters::SQLServerAdapter.new(logger,config.merge(:mode=>mode))
     end
-    
+
     protected
-    
+
     def self.did_retry_sqlserver_connection(connection,count)
       logger.info "CONNECTION RETRY: #{connection.class.name} retry ##{count}."
     end
-    
+
     def self.did_lose_sqlserver_connection(connection)
       logger.info "CONNECTION LOST: #{connection.class.name}"
     end
-    
+
   end
-  
+
   module ConnectionAdapters
-    
+
     class SQLServerColumn < Column
 
       def initialize(name, default, sql_type = nil, null = true, sqlserver_options = {})
@@ -55,35 +55,35 @@ module ActiveRecord
         super(name, default, sql_type, null)
         @primary = @sqlserver_options[:is_identity]
       end
-      
+
       class << self
-        
+
         def string_to_binary(value)
          "0x#{value.unpack("H*")[0]}"
         end
-        
+
         def binary_to_string(value)
           value =~ /[^[:xdigit:]]/ ? value : [value].pack('H*')
         end
-        
+
       end
-      
+
       def is_identity?
         @sqlserver_options[:is_identity]
       end
-      
+
       def is_utf8?
         !!(@sql_type =~ /nvarchar|ntext|nchar/i)
       end
-      
+
       def is_integer?
         !!(@sql_type =~ /int/i)
       end
-      
+
       def is_real?
         !!(@sql_type =~ /real/i)
       end
-      
+
       def sql_type_for_statement
         if is_integer? || is_real?
           sql_type.sub(/\(\d+\)/,'')
@@ -91,15 +91,15 @@ module ActiveRecord
           sql_type
         end
       end
-      
+
       def default_function
         @sqlserver_options[:default_function]
       end
-      
+
       def table_name
         @sqlserver_options[:table_name]
       end
-      
+
       def table_klass
         @table_klass ||= begin
           table_name.classify.constantize
@@ -108,14 +108,14 @@ module ActiveRecord
         end
         (@table_klass && @table_klass < ActiveRecord::Base) ? @table_klass : nil
       end
-      
+
       def database_year
         @sqlserver_options[:database_year]
       end
-      
-      
+
+
       private
-      
+
       def extract_limit(sql_type)
         case sql_type
         when /^smallint/i
@@ -130,7 +130,7 @@ module ActiveRecord
           super
         end
       end
-      
+
       def simplified_type(field_type)
         case field_type
           when /real/i              then :float
@@ -144,7 +144,7 @@ module ActiveRecord
           else super
         end
       end
-      
+
       def simplified_datetime
         if database_year >= 2008
           :datetime
@@ -156,38 +156,38 @@ module ActiveRecord
           :datetime
         end
       end
-      
+
     end #class SQLServerColumn
-    
+
     class SQLServerAdapter < AbstractAdapter
-      
+
       include Sqlserver::Quoting
       include Sqlserver::DatabaseStatements
       include Sqlserver::SchemaStatements
       include Sqlserver::DatabaseLimits
       include Sqlserver::Errors
       include Sqlserver::Version
-      
+
       ADAPTER_NAME                = 'SQLServer'.freeze
       DATABASE_VERSION_REGEXP     = /Microsoft SQL Server\s+"?(\d{4}|\w+)"?/
-      SUPPORTED_VERSIONS          = [2005,2008,2010,2011].freeze
-      
+      SUPPORTED_VERSIONS          = [2000,2005,2008,2010,2011].freeze
+
       attr_reader :database_version, :database_year
-      
+
       cattr_accessor :native_text_database_type, :native_binary_database_type, :native_string_database_type,
                      :log_info_schema_queries, :enable_default_unicode_types, :auto_connect,
                      :cs_equality_operator, :lowercase_schema_reflection, :auto_connect_duration
-      
+
       self.enable_default_unicode_types = true
-      
+
       class << self
-        
+
         def visitor_for(pool)
           Arel::Visitors::SQLServer.new(pool)
         end
-        
+
       end
-      
+
       def initialize(logger,config)
         @connection_options = config
         connect
@@ -211,42 +211,42 @@ module ActiveRecord
           raise NotImplementedError, "Currently, only #{SUPPORTED_VERSIONS.to_sentence} are supported. We got back #{@database_version}."
         end
       end
-      
+
       # === Abstract Adapter ========================================== #
-      
+
       def adapter_name
         ADAPTER_NAME
       end
-      
+
       def supports_migrations?
         true
       end
-      
+
       def supports_primary_key?
         true
       end
-      
+
       def supports_count_distinct?
         true
       end
-      
+
       def supports_ddl_transactions?
         true
       end
-      
+
       def supports_savepoints?
         true
       end
-      
+
       def disable_referential_integrity
         do_execute "EXEC sp_MSforeachtable 'ALTER TABLE ? NOCHECK CONSTRAINT ALL'"
         yield
       ensure
         do_execute "EXEC sp_MSforeachtable 'ALTER TABLE ? CHECK CONSTRAINT ALL'"
       end
-      
+
       # === Abstract Adapter (Connection Management) ================== #
-      
+
       def active?
         case @connection_options[:mode]
         when :dblib
@@ -272,17 +272,17 @@ module ActiveRecord
           @connection.disconnect rescue nil
         end
       end
-      
+
       def reset!
         remove_database_connections_and_rollback { }
       end
-      
+
       def clear_cache!
         initialize_sqlserver_caches
       end
-      
+
       # === Abstract Adapter (Misc Support) =========================== #
-      
+
       def pk_and_sequence_for(table_name)
         idcol = identity_column(table_name)
         idcol ? [idcol.name,nil] : nil
@@ -291,74 +291,74 @@ module ActiveRecord
       def primary_key(table_name)
         identity_column(table_name).try(:name)
       end
-      
+
       # === SQLServer Specific (DB Reflection) ======================== #
-      
+
       def sqlserver?
         true
       end
-      
+
       def sqlserver_2005?
         @database_year == 2005
       end
-      
+
       def sqlserver_2008?
         @database_year == 2008
       end
-      
+
       def sqlserver_2011?
         @database_year == 2011
       end
-      
+
       def sqlserver_azure?
         @sqlserver_azure
       end
-      
+
       def version
         self.class::VERSION
       end
-      
+
       def inspect
         "#<#{self.class} version: #{version}, year: #{@database_year}, connection_options: #{@connection_options.inspect}>"
       end
-      
+
       def auto_connect
         @@auto_connect.is_a?(FalseClass) ? false : true
       end
-      
+
       def auto_connect_duration
         @@auto_connect_duration ||= 10
       end
-      
+
       def native_string_database_type
-        @@native_string_database_type || (enable_default_unicode_types ? 'nvarchar' : 'varchar') 
+        @@native_string_database_type || (enable_default_unicode_types ? 'nvarchar' : 'varchar')
       end
-      
+
       def native_text_database_type
         @@native_text_database_type || enable_default_unicode_types ? 'nvarchar(max)' : 'varchar(max)'
       end
-      
+
       def native_time_database_type
         sqlserver_2005? ? 'datetime' : 'time'
       end
-      
+
       def native_date_database_type
         sqlserver_2005? ? 'datetime' : 'date'
       end
-      
+
       def native_binary_database_type
         @@native_binary_database_type || 'varbinary(max)'
       end
-      
+
       def cs_equality_operator
         @@cs_equality_operator || 'COLLATE Latin1_General_CS_AS_WS'
       end
-      
-      
+
+
       protected
-      
+
       # === Abstract Adapter (Misc Support) =========================== #
-      
+
       def translate_exception(e, message)
         case message
         when /cannot insert duplicate key .* with unique index/i
@@ -371,9 +371,9 @@ module ActiveRecord
           super
         end
       end
-      
+
       # === SQLServer Specific (Connection Management) ================ #
-      
+
       def connect
         config = @connection_options
         @connection = case @connection_options[:mode]
@@ -382,7 +382,7 @@ module ActiveRecord
                         login_timeout = config[:login_timeout].present? ? config[:login_timeout].to_i : nil
                         timeout = config[:timeout].present? ? config[:timeout].to_i/1000 : nil
                         encoding = config[:encoding].present? ? config[:encoding] : nil
-                        TinyTds::Client.new({ 
+                        TinyTds::Client.new({
                           :dataserver    => config[:dataserver],
                           :host          => config[:host],
                           :port          => config[:port],
@@ -418,7 +418,7 @@ module ActiveRecord
                           ODBC::Database.new.drvconnect(driver)
                         else
                           ODBC.connect config[:dsn], config[:username], config[:password]
-                        end.tap do |c|  
+                        end.tap do |c|
                           begin
                             c.use_time = true
                             c.use_utc = ActiveRecord::Base.default_timezone == :utc
@@ -430,7 +430,7 @@ module ActiveRecord
       rescue
         raise unless @auto_connecting
       end
-      
+
       def initialize_dateformatter
         @database_dateformat = user_options['dateformat']
         a, b, c = @database_dateformat.each_char.to_a
@@ -439,7 +439,7 @@ module ActiveRecord
         ::Date::DATE_FORMATS[:_sqlserver_dateformat] = dateformat
         ::Time::DATE_FORMATS[:_sqlserver_dateformat] = dateformat
       end
-      
+
       def remove_database_connections_and_rollback(database=nil)
         database ||= current_database
         do_execute "ALTER DATABASE #{quote_table_name(database)} SET SINGLE_USER WITH ROLLBACK IMMEDIATE"
@@ -449,7 +449,7 @@ module ActiveRecord
           do_execute "ALTER DATABASE #{quote_table_name(database)} SET MULTI_USER"
         end if block_given?
       end
-      
+
       def with_auto_reconnect
         begin
           yield
@@ -458,7 +458,7 @@ module ActiveRecord
           raise
         end
       end
-      
+
       def auto_reconnected?
         return false unless auto_connect
         @auto_connecting = true
@@ -474,10 +474,10 @@ module ActiveRecord
       ensure
         @auto_connecting = false
       end
-            
+
     end #class SQLServerAdapter < AbstractAdapter
-    
+
   end #module ConnectionAdapters
-  
+
 end #module ActiveRecord
 
